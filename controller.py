0eb52cc30f8aae12ac5a7818db8e38cd721c5a14
julikqr <jlsch@gmx.de>
2021-04-30 19:44:10 +0200

    added crud buttons to gui

--- controller.py
+++ controller.py
@@ -1,7 +1,12 @@
+from model import Model
+from view import View
+
+
 class Controller:
-    def __init__():
-        pass
+    def __init__(self):
+        print("Hello from Controller")
+        self.model = Model()
+        self.view = View()
 
-
-if __name__ == "__main__":
-    pass
+    def start_gui(self):
+        self.view.start_gui()
